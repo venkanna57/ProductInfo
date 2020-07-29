f5135edbc6a85f7a358161ae9ed3c3cf5a95f1c0
venkanna57 <gvs086@gmail.com>
2020-07-29 11:34:46 +0530

    initial setup

---
+++ src/main/java/com/hcl/product/productinfo/ProductinfoApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.product.productinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/product/productinfo/ProductinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.product.productinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
